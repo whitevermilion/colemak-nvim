0d959f43e2c2c83eaaaa1a5080f1108c66e4bcc3
WhiteVermilion <whitevermilion@qq.com>
2025-10-18 13:48:34 +0800

    删除了c,cpp的lsp的单独的配置文件，删除了lsp里面的跳转快捷键，简化了配置

--- lua/extras/extras_list.lua
+++ lua/extras/extras_list.lua
@@ -1,7 +1,7 @@
 -- lua/extras/extras_list.lua
 return {
   { import = "extras/dap" }, -- 调试功能
-  { import = "extras/lang/c" }, -- C语言支持
+  --   { import = "extras/lang/c" }, -- C语言支持
   { import = "extras/lang/md" }, -- Markdown增强
   --   { import = "extras/ai" }, -- AI编程助手
   -- { import = "extras/todo-list" }, -- 任务管理（注释表示禁用）
