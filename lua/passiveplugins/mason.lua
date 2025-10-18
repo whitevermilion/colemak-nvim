4dcfe77850f78cd8e6b1069c92a34e5e58523600
WhiteVermilion <whitevermilion@qq.com>
2025-10-18 16:17:46 +0800

    删除了lsp-config插件，使用nvim的内置lsp功能

---
+++ lua/passiveplugins/mason.lua
@@ -0,0 +1,13 @@
+-- lua/passiveplugins/mason.lua
+
+return {
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup({
+        ui = { border = "rounded" },
+      })
+    end,
+  },
+}
