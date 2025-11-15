999259893cd50420aee05d4a50b89abd988d7cb6
WhiteVermilion <whitevermilion@qq.com>
2025-11-15 22:49:55 +0800

    添加了html文件格式化功能

--- lua/passiveplugins/lsp_conform.lua
+++ lua/passiveplugins/lsp_conform.lua
@@ -8,26 +8,66 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        -- 为 Markdown 文件启用注入式格式化器
-        markdown = { "prettier", "injected" },
+        markdown = { "prettier", "injected" }, -- 只为 markdown 启用注入
+        html = { "prettier" },
+        htm = { "prettier" },
+        css = { "prettier" },
+        javascript = { "prettier" },
         c = { "clang_format" },
         cpp = { "clang_format" },
         lua = { "stylua" },
-        -- python = { "black", "isort" },
       },
 
-      -- 添加格式化器配置
       formatters = {
+        prettier = {
+          command = "prettier",
+          args = function(self, ctx)
+            -- 根据文件扩展名确定解析器
+            local ext = vim.fn.fnamemodify(ctx.filename or "", ":e")
+            local parser
+
+            if ext == "html" or ext == "htm" then
+              parser = "html"
+            elseif ext == "css" then
+              parser = "css"
+            elseif ext == "md" or ext == "markdown" then
+              parser = "markdown"
+            elseif ext == "js" or ext == "javascript" then
+              parser = "babel"
+            else
+              parser = "html" -- 默认使用 html 解析器
+            end
+
+            return {
+              "--stdin-filepath",
+              ctx.filename or "unknown.html",
+              "--parser",
+              parser,
+              "--tab-width",
+              "2",
+              "--use-tabs",
+              "false",
+              "--print-width",
+              "80",
+              "--html-whitespace-sensitivity",
+              "css",
+              "--end-of-line",
+              "lf",
+            }
+          end,
+        },
         stylua = {
-          args = { "--indent-type", "Spaces", "--indent-width", "2", "-" },
+          command = "stylua",
+          args = { "-" }, -- 使用默认配置
         },
         clang_format = {
+          command = "clang-format",
           args = { "--style={BasedOnStyle: Google, UseTab: false, IndentWidth: 4, TabWidth: 4}" },
         },
       },
     })
 
-    -- 为特定语言配置注入的格式化器
+    -- 为 markdown 配置注入的格式化器
     require("conform").formatters.injected = {
       options = {
         lang_to_formatters = {
@@ -35,19 +75,25 @@ return {
           python = { "black" },
           c = { "clang_format" },
           cpp = { "clang_format" },
+          html = { "prettier" },
+          css = { "prettier" },
+          javascript = { "prettier" },
         },
       },
     }
 
-    -- 自动格式化（在保存时）
+    -- 自动格式化
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*",
       callback = function(args)
-        conform.format({
-          bufnr = args.buf,
-          timeout_ms = 3000,
-          async = false,
-        })
+        local bufname = vim.api.nvim_buf_get_name(args.buf)
+        if bufname ~= "" then
+          conform.format({
+            bufnr = args.buf,
+            timeout_ms = 5000,
+            async = false,
+          })
+        end
       end,
     })
   end,
